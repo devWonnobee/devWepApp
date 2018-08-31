bf1616756670552da2e609bb20c97bf9931af89c
devWonnobee <dev.micsaltat@gmail.com>
2018-08-31 22:35:36 +0200

    I added a route for login pages

--- views.py
+++ views.py
@@ -4,3 +4,16 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html', title='Homepage')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                        request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
